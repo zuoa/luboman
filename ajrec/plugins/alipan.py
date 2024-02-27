4413c304b6d0864027388d70ac6a9f29c177db19
yujian <yujian@airimos.com>
2024-02-27 17:02:30 +0800

    上传

--- ajrec/plugins/alipan.py
+++ ajrec/plugins/alipan.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import sys
 
+from ajrec.config import config
 from ajrec.core.decorators import PluginTool
 from ajrec.core.upload import Uploader
 
@@ -15,9 +16,12 @@ class Alipan(Uploader):
         super().__init__(file_list)
 
     def upload(self):
+
+        alipan_path = config.get('ffmpeg_path', '/usr/local/bin/aliyunpan')
+
         for file_info in self.file_list:
             drive_dir = os.path.dirname(file_info['video'])
-            command_args = ['/usr/local/bin/aliyunpan', 'upload', file_info['video'], drive_dir]
+            command_args = [alipan_path, 'upload', file_info['video'], drive_dir]
 
             try:
                 proc = subprocess.Popen(command_args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)

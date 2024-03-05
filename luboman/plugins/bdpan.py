f8c96b8ba30814930aa4ba722f0d052c95798396
yujian <yujian@airimos.com>
2024-03-05 21:55:06 +0800

    1

--- luboman/plugins/bdpan.py
+++ luboman/plugins/bdpan.py
@@ -16,7 +16,7 @@ class Baidupan(Uploader):
         super().__init__(file_list)
 
     def upload(self):
-        bp = ByPy(debug=True)
+        bp = ByPy()
         for file_info in self.file_list:
             drive_dir = os.path.dirname(file_info['video'])
             bp.mkdir(drive_dir)

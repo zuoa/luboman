ee2ab83aecbf40db87ec58913d03bb8b68e37ff5
“yujian” <yujian@airimos.com>
2024-03-08 08:16:41 +0800

    1

--- luboman/config.py
+++ luboman/config.py
@@ -11,28 +11,14 @@ logger = logging.getLogger("luboman")
 
 class Config(UserDict):
     def load_from_db(self):
-        context = {
-            'url_upload_count': self.data.get('url_upload_count', {}),
-            'upload_filename': self.data.get('upload_filename', []),
-            'PluginInfo': self.data.get('PluginInfo')
-        }
+        context = {}
         for cfg in GlobalConfig.select():
             self.data[cfg.key] = cfg.value
 
         self.data.update(context)
 
         logger.info(f"Config: {self.data}")
-        #
-        # self['streamers'] = {}
-        # for ls in LiveStreamers.select():
-        #     self['streamers'][ls.remark] = {k: v for k, v in model_to_dict(ls).items() if v and (k != 'upload_streamers')}
-        #     # self['streamers'][ls.remark].pop('upload_streamers')
-        #     if ls.upload_streamers:
-        #         self['streamers'][ls.remark].update({k: v for k, v in model_to_dict(ls.upload_streamers).items() if v})
-        #     if self['streamers'][ls.remark].get('tags'):
-        #         self['streamers'][ls.remark]['tags'] = self['streamers'][ls.remark]['tags']
-        # for us in UploadStreamers.select():
-        #     config.data[con.key] = con.value
+
 
     def save_to_db(self):
         with db.connection_context():

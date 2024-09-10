a0b454fc7722695e9c75bc5dd7eb8f476b133c50
yujian <yujian@airimos.com>
2024-09-10 10:27:40 +0800

    1

--- luboman/core/utils.py
+++ luboman/core/utils.py
@@ -51,12 +51,35 @@ def format_live_prop_text(formatted_str: str, room_data):
     return prop_text
 
 
+def get_project_rootpath():
+    """
+    获取项目根目录。此函数的能力体现在，不论当前module被import到任何位置，都可以正确获取项目根目录
+    :return:
+    """
+    path = os.path.realpath(os.curdir)
+    while True:
+        # PyCharm项目中，'.idea'是必然存在的，且名称唯一
+        if '.idea' in os.listdir(path):
+            return path
+        path = os.path.dirname(path)
+
+
+def get_data_dir():
+    return '/data' if os.path.exists('/.dockerenv') else f'{get_project_rootpath()}/data'
+
+
 def get_video_dir():
-    return '/data/video' if os.path.exists('/.dockerenv') else 'data/video'
+    path = os.path.join(get_data_dir(), 'video')
+    os.makedirs(path, exist_ok=True)
+
+    return path
 
 
 def get_public_dir():
-    return '/data/public' if os.path.exists('/.dockerenv') else 'data/public'
+    path = os.path.join(get_data_dir(), 'public')
+    os.makedirs(path, exist_ok=True)
+
+    return path
 
 
 def remove_filelist(file_list):
